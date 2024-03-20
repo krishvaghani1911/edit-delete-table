ea0760de26a7ecd2483f3a8b17e9dd696c1b0198
krish vaghani <vaghanikrish1911@gmail.com>
2024-03-20 15:55:23 +0530

    new start up

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      hello
+      halo
     </>
   )
 }
